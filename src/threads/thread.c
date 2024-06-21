9c7e1470e95d004f9a0ad1b12f502680299972b5
belc <belc@hwc13.cin.ufpe.br>
2024-06-21 17:03:39 -0300

    commit teste

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -315,6 +315,22 @@ thread_yield (void)
   intr_set_level (old_level);
 }
 
+void
+thread_yield_2 (void) 
+{
+  struct thread *cur = thread_current ();
+  enum intr_level old_level;
+  
+  ASSERT (!intr_context ());
+
+  old_level = intr_disable ();
+  if (cur != idle_thread) 
+    list_push_back (&ready_list, &cur->elem);
+  cur->status = THREAD_READY;
+  schedule ();
+  intr_set_level (old_level);
+}
+
 /* Invoke function 'func' on all threads, passing along 'aux'.
    This function must be called with interrupts off. */
 void
